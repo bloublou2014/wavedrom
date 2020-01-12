b79ec1578b50a7148882b66b0bc7e8a0244e8aad
Alexandre <alexandre.guillon@mckay-brothers.com>
2020-01-12 08:38:18 +0100

    Update svg2skin

--- skins/unpacked/README.md
+++ skins/unpacked/README.md
@@ -5,7 +5,7 @@ Effectively the skin contains building blocks (bricks) of different parts of wav
 Each brick is an SVG group with unique id. WaveDrom engine will include template into
 every diagram picture and build picture referencing to elements with specific ids.
 
-[default.svg](https://github.com/drom/wavedrom/blob/master/unpacked/skins/default.svg)
+[default.svg](./skins/default.svg)
 
 ### Drawing the skin
 
@@ -13,18 +13,19 @@ User can define own skin in order to change expression properties of resulted ti
 [http://inkscape.org/ Inkscape] editor can be used to draw the skin.
 
 See 
-[default.svg](https://github.com/drom/wavedrom/blob/master/unpacked/skins/default.svg) and
-[narrow.svg](https://github.com/drom/wavedrom/blob/master/unpacked/skins/narrow.svg)
+[default.svg](./skins/default.svg) and
+[narrow.svg](./skins/narrow.svg)
 source for example.
 
 ### Packing the skin
 
 The source skin SVG need to be converted by
-[bin/svg2js.js](https://github.com/drom/wavedrom/blob/master/bin/svg2js.js)
+[svg2skin.js](./svg2skin.js)
 script in order to be used by WaveDrom.
 
 The resulted JSON file much smaller in size and has meta-information needed for WaveDrom to work.
 
 See the example of packed skin:
-[default.js](https://github.com/drom/wavedrom/blob/master/skins/default.js)
-[narrow.js](https://github.com/drom/wavedrom/blob/master/skins/narrow.js)
+- [default.js](../default.js) / [default.css](../default.css) 
+- [lowkey.js](../lowkey.js) / [lowkey.css](../lowkey.css) 
+- [narrow.js](../narrow.js) / [narrow.css](../narrow.css) 
---
+++ skins/unpacked/svg2skin.js
@@ -0,0 +1,146 @@
+'use strict';
+// Convert SVG unpacked skin version to skin file
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+const {parse} = require('svgson');
+const CleanCSS = require('clean-css');
+const Mustache = require('mustache');
+const json5 = require('json5');
+
+
+// region constants
+const skinTpl = fs.readFileSync(path.join(__dirname, 'skin.mustache'), 'utf8');
+const defaultCss = 'text{font-size:11pt;font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;text-align:center;fill-opacity:1;font-family:Helvetica}.muted{fill:#aaa}.warning{fill:#f6b900}.error{fill:#f60000}.info{fill:#0041c4}.success{fill:#00ab00}.h1{font-size:33pt;font-weight:bold}.h2{font-size:27pt;font-weight:bold}.h3{font-size:20pt;font-weight:bold}.h4{font-size:14pt;font-weight:bold}.h5{font-size:11pt;font-weight:bold}.h6{font-size:8pt;font-weight:bold}';
+const defaultArrows = {
+    arrowhead: {type: 'marker', 'style': 'fill:#0041c4', 'markerHeight': '7', 'markerWidth': '10', 'markerUnits': 'strokeWidth', 'viewBox': '0 -4 11 8', 'refX': '15', 'refY': '0', 'orient': 'auto', svg: [['path', {'d': 'M0 -4 11 0 0 4z'}]]},
+    arrowtail: {type: 'marker', 'style': 'fill:#0041c4', 'markerHeight': '7', 'markerWidth': '10', 'markerUnits': 'strokeWidth', 'viewBox': '-11 -4 11 8', 'refX': '-15', 'refY': '0', 'orient': 'auto', svg: [['path', {'d': 'M0 -4 -11 0 0 4z'}]]}
+};
+// endregion
+
+// region SVG Conversion
+function convertSvg(json) {
+    const converted = {};
+    const cssClasses = [];
+    _.each(json.children, element => {
+        const res = convertSvgElement(element, cssClasses);
+        if (!res) {
+            return;
+        }
+        converted[res.id] = res.item;
+    });
+    // add defaults
+    _.each(defaultArrows, (element, id) => {
+        converted[id] = element;
+    });
+    return {converted: converted, cssClasses: cssClasses};
+}
+
+function convertSvgElement(element, cssClasses) {
+    const attrs = element.attributes;
+    if (['g', 'rect'].indexOf(element.name) < 0 || !_.has(attrs, 'id')) {
+        return;
+    }
+    const children = element.children;
+    const item = {
+        type: element.name,
+        svg: convertChildrenSvg(attrs.id, cssClasses, children)
+    };
+    if (attrs.id === 'socket') {
+        const first = children[0];
+        item.size = {
+            x: first.attributes.x,
+            y: first.attributes.y,
+            height: first.attributes.height,
+            width: first.attributes.width
+        };
+    }
+    return {id: attrs.id, item: item};
+}
+
+function getCssClass(cssClasses, css) {
+    let index = cssClasses.indexOf(css);
+    if (index < 0) {
+        index = cssClasses.length;
+        cssClasses.push(css);
+    }
+    return 's' + index;
+}
+
+function convertChildrenSvg(id, cssClasses, children) {
+    const childrenSvg = [];
+    _.each(children, (svgItem, i) => {
+        const attrsItem = svgItem.attributes;
+        if (!_.has(attrsItem, 'd')) {
+            return;
+        }
+        const res = {'d': attrsItem.d};
+        if (_.has(attrsItem, 'style')) {
+            res.class = getCssClass(cssClasses, attrsItem.style);
+        }
+        childrenSvg.push([svgItem.name, res]);
+    });
+    return childrenSvg;
+}
+
+// endregion
+
+// region Css optimization
+
+// optimize CSS classes to use the same classes for different svg element classes
+// avoiding using 's[1-9]' format > stick to the svg element's id to quickly find them back
+function optimizeCssClasses(cssClasses) {
+    // reverse dict
+    const fullCss = _.map(cssClasses, (css, id) => {
+        return '.s' + id + ' {' + css + '}';
+    }).join('\n');
+    // reverse cssClasses => same classes will fall to the same bucket
+    return new CleanCSS({format: 'beautify'}).minify(defaultCss + '\n' + fullCss);
+}
+
+// endregion
+
+// stringify with '
+function stringify(element) {
+    const res = json5.stringify(element, {
+        quote: '\''
+    });
+    return res.replace(/},{/g, '}, {');
+}
+
+const unpackedSkinDirectory = path.join(__dirname, 'skins');
+const skinFiles = fs.readdirSync(unpackedSkinDirectory);
+_.each(skinFiles, filename => {
+    const svgContent = fs.readFileSync(path.join(unpackedSkinDirectory, filename), 'utf8').toString();
+    parse(svgContent)
+        .then(json => {
+            // convert svg to id:converted, and cssClasses
+            return convertSvg(json);
+        })
+        .then(res => {
+            // generate file content
+            const css = optimizeCssClasses(res.cssClasses);
+            const name = path.parse(filename).name;
+            const converted = res.converted;
+            // format quickly defs
+            let defs = _.map(converted, (element, id) => {
+                return {id: id, value: stringify(element)};
+            });
+            defs = _.orderBy(defs, ['id'], ['asc']);
+            const info = {
+                name: name,
+                socketSize: converted.socket ? stringify(converted.socket.size) : undefined,
+                defs: defs
+            };
+            const skin = Mustache.render(skinTpl, info);
+            return {name: name, css: css, skin: skin};
+        })
+        .then(res => {
+            // write files
+            const skinDirectory = path.join(__dirname, '..');
+            const cssFilename = path.join(skinDirectory, res.name + '.css');
+            const skinFilename = path.join(skinDirectory, res.name + '.js');
+            fs.writeFileSync(cssFilename, res.css.styles);
+            fs.writeFileSync(skinFilename, res.skin);
+        });
+});
